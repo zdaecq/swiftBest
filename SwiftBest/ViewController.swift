137ab6f7e9b04ef8d0680117781796354ca82efe
Yaroslav Bondar <bonyadmitr@gmail.com>
2016-06-20 22:51:13 +0300

    Initial Commit

---
+++ SwiftBest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftBest
+//
+//  Created by zdaecqze zdaecq on 20.06.16.
+//  Copyright © 2016 Bondar Yaroslav. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
