79e55314d6e2caf0eb392a9956f46adb58398b39
Bondar Yaroslav <ybondar@smedialink.com>
2016-06-24 17:30:20 +0300

    changes 2

---
+++ SwiftBest/swiftMy/ExtentionsOthers/PromiseExtnetions.swift
@@ -0,0 +1,22 @@
+//
+//  PromiseExtnetions.swift
+//  OrderApp-Customer-iOS
+//
+//  Created by Yaroslav Bondar on 24.06.16.
+//  Copyright © 2016 SMediaLink. All rights reserved.
+//
+
+// TODO: Need to import PromiseKit framwork to project
+
+//import UIKit
+//import PromiseKit
+//
+//extension Promise {
+//    func catchAndShow() {
+//        self.error { (error) in
+//            if let error = error as? Translatable {
+//                UIAlertView(title: "Error", message: error.translated(locale: NSLocale(localeIdentifier: "EN")), delegate: nil, cancelButtonTitle: "OK").show()
+//            }
+//        }
+//    }
+//}
