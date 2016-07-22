ac2cd56a3626786648b35fb9e3405a8ab95c224d
Bondar Yaroslav <ybondar@smedialink.com>
2016-07-22 10:50:23 +0300

    UIViewController + Child

---
+++ SwiftBest/swiftMy/Extentions/UIViewController + Child.swift
@@ -0,0 +1,41 @@
+//
+//  UIViewController + Child.swift
+//  MenuDrawer
+//
+//  Created by Yaroslav Bondar on 20.07.16.
+//  Copyright © 2016 SMediaLink. All rights reserved.
+//
+
+import UIKit
+
+extension UIViewController {
+    
+    // container must be view of UIViewController
+    func addChildVC(vc: UIViewController, toContainer container: UIView) {
+        
+        vc.willMoveToParentViewController(self)
+        
+        vc.view.frame = container.bounds
+        vc.view.layoutIfNeeded()
+        
+        container.removeSubviews()
+        container.addSubview(vc.view)
+        
+        self.addChildViewController(vc)
+        vc.didMoveToParentViewController(self)
+    }
+    
+    func addChildVC(vc: UIViewController) {
+        addChildVC(vc, toContainer: self.view)
+    }
+    
+    func addChildWithNavItem(vc: UIViewController, toContainer container: UIView) {
+        addChildVC(vc, toContainer: container)
+        navigationItem.rightBarButtonItems = vc.navigationItem.rightBarButtonItems
+        title = vc.title
+    }
+    
+    func addChildVCWithNavItem(vc: UIViewController) {
+        addChildWithNavItem(vc, toContainer: self.view)
+    }
+}
