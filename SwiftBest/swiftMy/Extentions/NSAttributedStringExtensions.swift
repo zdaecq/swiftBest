ad5cab0fa5542be65d70ee4bb905b2051c43ab5c
Yaroslav Bondar <bonyadmitr@gmail.com>
2016-08-10 22:48:43 +0300

    small comment

--- SwiftBest/swiftMy/Extentions/NSAttributedStringExtensions.swift
+++ SwiftBest/swiftMy/Extentions/NSAttributedStringExtensions.swift
@@ -9,13 +9,13 @@
 import UIKit
 
 extension NSAttributedString {
-    
+
     class func getText(text: String, withImage imageName: String) -> NSAttributedString {
-        
+
         let imageAttachment = ImageTextAttachment(data: nil, ofType: nil)
         imageAttachment.image = UIImage(named: imageName)
         let imageString = NSAttributedString(attachment: imageAttachment)
-        
+
         let newString = NSMutableAttributedString(attributedString: imageString)
         let attributedText = NSAttributedString(string: text)
         newString.appendAttributedString(attributedText)
@@ -25,15 +25,30 @@ extension NSAttributedString {
 
 
 class ImageTextAttachment: NSTextAttachment {
-    
+
     override func attachmentBoundsForTextContainer(textContainer: NSTextContainer?, proposedLineFragment lineFrag: CGRect, glyphPosition position: CGPoint, characterIndex charIndex: Int) -> CGRect {
-        
-        // TODO: Fix this class
+
+        // TODO: Fix this class. use comented code below
         let image = UIImage(named: "")
         let width = (image?.size.width)!
         let height = (image?.size.height)!
         let newBounds = CGRect(x: 0, y: -1.5, width: width, height: height)
-        return newBounds;
+        return newBounds
     }
 }
 
+//let textView = UITextView(frame: CGRectMake(50, 50, 200, 300))
+//let attributedString = NSMutableAttributedString(string: "before after")
+//let textAttachment = NSTextAttachment()
+//textAttachment.image = UIImage(named: "sample_image.jpg")!
+
+//let oldWidth = textAttachment.image!.size.width;
+
+////I'm subtracting 10px to make the image display nicely, accounting
+////for the padding inside the textView
+//let scaleFactor = oldWidth / (textView.frame.size.width - 10);
+//textAttachment.image = UIImage(CGImage: textAttachment.image!.CGImage, scale: scaleFactor, orientation: .Up)
+//var attrStringWithImage = NSAttributedString(attachment: textAttachment)
+//attributedString.replaceCharactersInRange(NSMakeRange(6, 1), withAttributedString: attrStringWithImage)
+//textView.attributedText = attributedString;
+//self.view.addSubview(textView)
