9a8d4fa12a517fff9fc9826a63e767dc83b97782
Yaroslav Bondar <bonyadmitr@gmail.com>
2016-08-21 00:02:59 +0300

    get current controller, when application move to background

--- SwiftBest/AppDelegate.swift
+++ SwiftBest/AppDelegate.swift
@@ -15,19 +15,19 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     // DEEP LINK, URL Scheme method
     func application(application: UIApplication, openURL url: NSURL, sourceApplication: String?, annotation: AnyObject) -> Bool {
-        
+
         if url.host == nil {
-            return true;
+            return true
         }
-        
+
 //        let urlString = url.absoluteString
 //        let queryArray = urlString.componentsSeparatedByString("/")
 //        let query = queryArray[2]
-//        
+//
 //        if let controller = UIApplication.topController as? OCVerificationController {
 //            controller.verificationCodeTextField.text = query
 //        }
-        
+
         return true
     }
 
@@ -37,6 +37,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     func applicationWillResignActive(application: UIApplication) {
+        let controller = (UIApplication.topController as! UINavigationController).topViewController as! TableViewController
+        let cell = controller.tableView.visibleCells.first as! TableViewCell
+        print(cell.name.text)
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
     }
@@ -60,4 +63,3 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
 }
-
